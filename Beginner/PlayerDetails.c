d94dfd5ce9f44eeee9eed9dab8767af720d8ba79
kingsheril <34640986+kingsheril@users.noreply.github.com>
2017-12-18 13:03:40 +0530

    Add files via upload

---
+++ Beginner/PlayerDetails.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(void)
+{
+int matches=463;
+float runs=18426,avg=44.83;
+char name[25]="Sachin Ramesh Tendulkar",nick[25]="Little Master";
+printf("%s\n%s\n%f\n%f\n%d\n",name,nick,runs,avg,matches);
+return 0;
+}
+
